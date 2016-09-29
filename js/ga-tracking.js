37d6e91084fa31ff340761111ac9ad4746f18a10
adisak1618 <adisakchaiyakul@gmail.com>
2016-09-29 19:07:47 +0700

    fix tracking

--- js/ga-tracking.js
+++ js/ga-tracking.js
@@ -1,74 +1,77 @@
-function segmentv1_1() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_2() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_3() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_4() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_5() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_6() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_7() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_8() {analytics.track('{{this.price}}', {category: 'Camera',label: '{{this.name}}',value: 1});}
-function segmentv1_9() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_10() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_11() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_12() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_13() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_14() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_15() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_16() {analytics.track('{{this.price}}', {category: 'Bikes',label: '{{this.name}}',value: 1});}
-function segmentv1_17() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_18() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_19() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_20() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_21() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_22() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_23() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_24() {analytics.track('{{this.price}}', {category: 'Bags',label: '{{this.name}}',value: 1});}
-function segmentv1_25() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_26() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_27() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_28() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_29() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_30() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_31() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_32() {analytics.track('{{this.price}}', {category: 'VR',label: '{{this.name}}',value: 1});}
-function segmentv1_33() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_34() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_35() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_36() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_37() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_38() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_39() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_40() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_41() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_42() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_43() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_44() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_45() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_46() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_47() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_48() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_49() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_50() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_51() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_52() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_53() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_54() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_55() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_56() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_57() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_58() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_59() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_60() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_61() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_62() {analytics.track('{{this.price}}', {category: 'All',label: '{{this.name}}',value: 1});}
-function segmentv1_63() {analytics.track('ClickHome', {category: 'Homepage',label: 'Home',value: 1});}
-function segmentv1_64() {analytics.track('ClickHowItWorks', {category: 'Homepage',label: 'HowItWorks',value: 1});}
-function segmentv1_65() {analytics.track('ClickMap', {category: 'Homepage',label: 'Map',value: 1});}
-function segmentv1_66() {analytics.track('ClickStartBrowsing', {category: 'Homepage',label: 'StartBrowsing',value: 1});}
-function segmentv1_67() {analytics.track('ClickHashtag', {category: 'Homepage',label: 'All',value: 1});}
-function segmentv1_68() {analytics.track('ClickHashtag', {category: 'Homepage',label: 'Camera',value: 1});}
-function segmentv1_69() {analytics.track('ClickHashtag', {category: 'Homepage',label: 'Bike',value: 1});}
-function segmentv1_70() {analytics.track('ClickHashtag', {category: 'Homepage',label: 'Bag',value: 1});}
-function segmentv1_71() {analytics.track('ClickHashtag', {category: 'Homepage',label: 'VR',value: 1});}
+function segmentv1_1() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_2() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_3() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_4() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_5() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_6() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_7() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_8() {analytics.track('price', {category: 'Camera',label: 'name',value: 1});}
+function segmentv1_9() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_10() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_11() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_12() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_13() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_14() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_15() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_16() {analytics.track('price', {category: 'Bikes',label: 'name',value: 1});}
+function segmentv1_17() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_18() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_19() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_20() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_21() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_22() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_23() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_24() {analytics.track('price', {category: 'Bags',label: 'name',value: 1});}
+function segmentv1_25() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_26() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_27() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_28() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_29() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_30() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_31() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_32() {analytics.track('price', {category: 'VR',label: 'name',value: 1});}
+function segmentv1_33() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_34() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_35() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_36() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_37() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_38() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_39() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_40() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_41() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_42() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_43() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_44() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_45() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_46() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_47() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_48() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_49() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_50() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_51() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_52() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_53() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_54() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_55() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_56() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_57() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_58() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_59() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_60() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_61() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_62() {analytics.track('price', {category: 'All',label: 'name',value: 1});}
+function segmentv1_63() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_64() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_65() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_66() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_67() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_68() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_69() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_70() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_71() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_72() {analytics.track('price', {category: 'Homepage',label: 'name',value: 1});}
+function segmentv1_73() {analytics.track('price', {category: 'Popup',label: 'name',value: 1});}
+function segmentv1_74() {analytics.track('price', {category: 'Popup',label: 'name',value: 1});}
 window.onload = function() {
 if(document.getElementById('camera-1')) {document.getElementById('camera-1').onclick=segmentv1_1;}
 if(document.getElementById('camera-2')) {document.getElementById('camera-2').onclick=segmentv1_2;}
@@ -134,11 +137,14 @@ if(document.getElementById('all-29')) {document.getElementById('all-29').onclick
 if(document.getElementById('all-30')) {document.getElementById('all-30').onclick=segmentv1_62;}
 if(document.getElementById('home-button')) {document.getElementById('home-button').onclick=segmentv1_63;}
 if(document.getElementById('How-it-work')) {document.getElementById('How-it-work').onclick=segmentv1_64;}
-if(document.getElementById('map-explor-section')) {document.getElementById('map-explor-section').onclick=segmentv1_65;}
-if(document.getElementById('start-browsing-button')) {document.getElementById('start-browsing-button').onclick=segmentv1_66;}
-if(document.getElementById('all-button-hastag')) {document.getElementById('all-button-hastag').onclick=segmentv1_67;}
-if(document.getElementById('camera-button-hastag')) {document.getElementById('camera-button-hastag').onclick=segmentv1_68;}
-if(document.getElementById('bike-button-hastag')) {document.getElementById('bike-button-hastag').onclick=segmentv1_69;}
-if(document.getElementById('bag-button-hastag')) {document.getElementById('bag-button-hastag').onclick=segmentv1_70;}
-if(document.getElementById('vr-button-hastag')) {document.getElementById('vr-button-hastag').onclick=segmentv1_71;}
+if(document.getElementById('list-your-item')) {document.getElementById('list-your-item').onclick=segmentv1_65;}
+if(document.getElementById('map-explor-section')) {document.getElementById('map-explor-section').onclick=segmentv1_66;}
+if(document.getElementById('start-browsing-button')) {document.getElementById('start-browsing-button').onclick=segmentv1_67;}
+if(document.getElementById('all-button-hastag')) {document.getElementById('all-button-hastag').onclick=segmentv1_68;}
+if(document.getElementById('camera-button-hastag')) {document.getElementById('camera-button-hastag').onclick=segmentv1_69;}
+if(document.getElementById('bike-button-hastag')) {document.getElementById('bike-button-hastag').onclick=segmentv1_70;}
+if(document.getElementById('bag-button-hastag')) {document.getElementById('bag-button-hastag').onclick=segmentv1_71;}
+if(document.getElementById('vr-button-hastag')) {document.getElementById('vr-button-hastag').onclick=segmentv1_72;}
+if(document.getElementById('submit-list')) {document.getElementById('submit-list').onclick=segmentv1_73;}
+if(document.getElementById('submit-rent')) {document.getElementById('submit-rent').onclick=segmentv1_74;}
 }
