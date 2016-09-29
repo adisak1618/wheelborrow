0d0bcedc0811a17e8549d8f7ceb7a39f2967437e
adisak1618 <adisakchaiyakul@gmail.com>
2016-09-29 16:40:18 +0700

    create server node for pm2

--- js/data.js
+++ js/data.js
@@ -1,4 +1,126 @@
 var itemsdata = {
+  all: [
+    {
+      name: 'Bianchi Intenso Size 57',
+      price: '300/Day',
+      img: 'bikes/bike 1.jpg'
+    },
+    {
+      name: 'Oculus Rift',
+      price: '800/Day',
+      img: 'vr/vr 4.jpg'
+    },
+    {
+      name: 'Specialized Allez Sprint',
+      price: '200/Day',
+      img: 'bikes/bike 2.jpg'
+    },
+    {
+      name: 'Oculus Rift Set VR',
+      price: '750/Day',
+      img: 'vr/vr 6.jpg'
+    },
+    {
+      name: 'Timbuk 2 Waterproof Pack',
+      price: '300/Day',
+      img: 'bags/bag 1.jpg'
+    },
+    {
+      name: 'HTC Vive',
+      price: '1000/day',
+      img: 'vr/vr 2.jpg'
+    },
+    {
+      name: 'Trek 1.1 (2016)',
+      price: '200/Day',
+      img: 'bikes/bike 3.jpg'
+    },
+    {
+      name: 'Sony QX Lens',
+      price: '200/Day',
+      img: 'cameras/camera 5.jpg'
+    },
+    {
+      name: 'Panasonic LUMIX GF8',
+      price: '200/Day',
+      img: 'cameras/camera 1.jpg'
+    },
+    {
+      name: 'Samsung Gear 360 Camera',
+      price: '200/Day',
+      img: 'cameras/camera 6.jpg'
+    },
+    {
+      name: 'GoPro Hero 3+ Set',
+      price: '150/Day',
+      img: 'cameras/camera 3.jpeg'
+    },
+    {
+      name: 'Bobo VR + Joystick',
+      price: '50/Day',
+      img: 'vr/vr 1.jpg'
+    },
+    {
+      name: 'Xiaomi Yi Camera',
+      price: '100/Day',
+      img: 'cameras/camera 4.jpg'
+    },
+    {
+      name: 'GoPro Hero 3+',
+      price: '100/Day',
+      img: 'cameras/camera 2.jpg'
+    },
+    {
+      name: 'Kelty Redwing 50-liter',
+      price: '400/Day',
+      img: 'bags/bag 6.jpg'
+    },
+    {
+      name: 'Samsung Gear VR',
+      price: '200/Day',
+      img: 'vr/vr 5.jpg'
+    },
+    {
+      name: 'Helmet S-FIGHT JT-038',
+      price: '200/Day',
+      img: 'bikes/bike 4.jpeg'
+    },
+    {
+      name: 'Samsung Gear',
+      price: '150/Day',
+      img: 'vr/vr 3.jpg'
+    },
+    {
+      name: 'Travel and Day Pack Set',
+      price: '700/Day',
+      img: 'bags/bag 4.jpg'
+    },
+    {
+      name: 'Small Rimowa Blue Luggage',
+      price: '1000/Day',
+      img: 'bags/bag 5.jpg'
+    },
+    {
+      name: 'Bike Pump Vertical',
+      price: '50/Day',
+      img: 'bikes/bike 5.jpg'
+    },
+    {
+      name: 'Daypack +water tank',
+      price: '400/Day',
+      img: 'bags/bag 2.jpg'
+    },
+    {
+      name: 'KTM Strada RA 1.0',
+      price: '100/Day',
+      img: 'bikes/bike 6.jpg'
+    },
+    {
+      name: 'Rip Curl Backpack',
+      price: '200/Day',
+      img: 'bags/bag 3.jpg'
+    }
+  ],
   bikes: [
     {
       name: 'Bianchi Intenso Size 57',
@@ -77,7 +199,7 @@ var itemsdata = {
     {
       name: 'GoPro Hero 3+ Set',
       price: '150/Day',
-      img: 'cameras/camera 3.jpg'
+      img: 'cameras/camera 3.jpeg'
     },
     {
       name: 'Xiaomi Yi Camera',
