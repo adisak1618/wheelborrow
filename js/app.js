e0037b1dfaedb91ce9f1f0904da6d9bbf024404d
adisak1618 <adisakchaiyakul@gmail.com>
2016-09-29 15:25:38 +0700

    init code

---
+++ js/app.js
@@ -0,0 +1,123 @@
+var locations = [
+  {lat: 13.7375714, lng: 100.5021517},
+  {lat: 13.739254, lng: 100.5558497},
+  {lat: 13.7776709, lng: 100.5581593},
+  {lat: 13.8309393, lng: 100.5191849},
+  {lat: 13.7675714, lng: 100.5001517},
+  {lat: 13.6375714, lng: 100.4021517},
+  {lat: 13.6375714, lng: 100.4521517},
+  {lat: 13.6075714, lng: 100.4921517},
+  {lat: 13.7375714, lng: 100.3021517},
+  {lat: 13.7061368, lng: 100.6233907},
+  {lat: 13.6216978, lng: 100.6635198},
+  {lat: 13.591674, lng: 100.5988513},
+  {lat: 13.7330611, lng: 100.5385132},
+  {lat: 13.691674, lng: 100.5488513},
+  {lat: 13.651674, lng: 100.5688513}
+];
+
+function initApp () {
+  $('.listingyouitem_button').click(function () {
+    $('.modal.popup3')
+      .modal('show');
+  });
+  $('.signupbutton').click(function () {
+    $('.modal.popup1')
+      .modal('show');
+  });
+  $('#listitem .button').click(function () {
+    $('.modal.popup1')
+      .modal('show');
+  });
+
+  $('.popup1 .submit').click(function (e) {
+    $('.modal')
+      .modal('hide');
+    $('.popup2').modal('show');
+  });
+
+  $('.popup3 .submit').click(function (e) {
+    $('.modal')
+      .modal('hide');
+    $('.popup2').modal('show');
+  });
+
+
+  setTimeout(function(){
+    $('.special.cards .image').dimmer({
+      on: 'hover'
+    });
+  }, 1000);
+
+
+  $(document).ready(function() {
+    $('#pickupdate').pickadate({
+        format: 'mm/dd/yyyy',
+        formatSubmit: 'mm/dd/yyyy',
+        hiddenName: true
+    });
+
+    $('#returndate').pickadate({
+        format: 'mm/dd/yyyy',
+        formatSubmit: 'mm/dd/yyyy',
+        hiddenName: true
+    });
+    $('#map').css({'height': ($( window ).height()-203)});
+    $( window ).resize(function() {
+      // resize();
+      // console.log($( window ).height());
+      $('#map').css({'height': ($( window ).height()-203)});
+    });
+  });
+
+
+
+}
+
+function initialize() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 13.7375714, lng: 100.5021517},
+    scrollwheel: false,
+    zoom: 11
+  });
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
+
+  for(var i = 0; i < locations.length; i++) {
+    var marker = new google.maps.Marker({
+      animation: google.maps.Animation.DROP,
+      map: map,
+      shape: shape,
+      position: locations[i],
+      title:"Hello World!"
+    });
+  }
+
+  console.log(locations);
+}
+
+function getUrlVars()
+  {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+  }
+
+  function getParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
---
+++ js/data.js
@@ -0,0 +1,130 @@
+var itemsdata = {
+  bikes: [
+    {
+      name: 'Bianchi Intenso Size 57',
+      price: '300/Day',
+      img: 'bikes/bike 1.jpg'
+    },
+    {
+      name: 'Specialized Allez Sprint',
+      price: '200/Day',
+      img: 'bikes/bike 2.jpg'
+    },
+    {
+      name: 'Trek 1.1 (2016)',
+      price: '200/Day',
+      img: 'bikes/bike 3.jpg'
+    },
+    {
+      name: 'Helmet S-FIGHT JT-038',
+      price: '200/Day',
+      img: 'bikes/bike 4.jpeg'
+    },
+    {
+      name: 'Bike Pump Vertical',
+      price: '50/Day',
+      img: 'bikes/bike 5.jpg'
+    },
+    {
+      name: 'KTM Strada RA 1.0',
+      price: '100/Day',
+      img: 'bikes/bike 6.jpg'
+    }
+  ],
+  bags: [
+    {
+      name: 'Timbuk 2 Waterproof Pack',
+      price: '300/Day',
+      img: 'bags/bag 1.jpg'
+    },
+    {
+      name: 'Daypack +water tank',
+      price: '400/Day',
+      img: 'bags/bag 2.jpg'
+    },
+    {
+      name: 'Rip Curl Backpack',
+      price: '200/Day',
+      img: 'bags/bag 3.jpg'
+    },
+    {
+      name: 'Travel and Day Pack Set',
+      price: '700/Day',
+      img: 'bags/bag 4.jpg'
+    },
+    {
+      name: 'Small Rimowa Blue Luggage',
+      price: '1000/Day',
+      img: 'bags/bag 5.jpg'
+    },
+    {
+      name: 'Kelty Redwing 50-liter',
+      price: '400/Day',
+      img: 'bags/bag 6.jpg'
+    }
+  ],
+  camera: [
+    {
+      name: 'Panasonic LUMIX GF8',
+      price: '200/Day',
+      img: 'cameras/camera 1.jpg'
+    },
+    {
+      name: 'GoPro Hero 3+',
+      price: '100/Day',
+      img: 'cameras/camera 2.jpg'
+    },
+    {
+      name: 'GoPro Hero 3+ Set',
+      price: '150/Day',
+      img: 'cameras/camera 3.jpg'
+    },
+    {
+      name: 'Xiaomi Yi Camera',
+      price: '100/Day',
+      img: 'cameras/camera 4.jpg'
+    },
+    {
+      name: 'Sony QX Lens',
+      price: '200/Day',
+      img: 'cameras/camera 5.jpg'
+    },
+    {
+      name: 'Samsung Gear 360 Camera',
+      price: '200/Day',
+      img: 'cameras/camera 6.jpg'
+    }
+  ],
+  vr: [
+    {
+      name: 'Bobo VR + Joystick',
+      price: '50/Day',
+      img: 'vr/vr 1.jpg'
+    },
+    {
+      name: 'HTC Vive',
+      price: '1000/day',
+      img: 'vr/vr 2.jpg'
+    },
+    {
+      name: 'Samsung Gear',
+      price: '150/Day',
+      img: 'vr/vr 3.jpg'
+    },
+    {
+      name: 'Oculus Rift',
+      price: '800/Day',
+      img: 'vr/vr 4.jpg'
+    },
+    {
+      name: 'Samsung Gear VR',
+      price: '200/Day',
+      img: 'vr/vr 5.jpg'
+    },
+    {
+      name: 'Oculus Rift Set VR',
+      price: '750/Day',
+      img: 'vr/vr 6.jpg'
+    }
+  ]
+}
